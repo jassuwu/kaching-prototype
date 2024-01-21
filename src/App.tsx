4668d1b12d80c5eb1c6ff3231a1813e237429c42
jassuwu <karthickprnv@gmail.com>
2024-01-21 17:45:55 +0530

    feat: init & complete metamask flow

---
+++ src/App.tsx
@@ -0,0 +1,116 @@
+import { Contract, parseUnits } from "ethers";
+import { BrowserProvider } from "ethers";
+import { useEffect, useState } from "react";
+import { USDC_ABI, USDC_MUMBAI_CONTRACT_ADDRESS } from "./constants";
+import { Loader } from "./components/Loader";
+
+function App() {
+  const [isProviderInjected, setIsProviderInjected] = useState(false);
+  const [provider, setProvider] = useState<BrowserProvider | null>(null);
+  const [walletAddress, setWalletAddress] = useState("");
+  const [receipt, setReceipt] = useState("");
+  const [txnPending, setTxnPending] = useState(false);
+
+  useEffect(() => {
+    const onLoad = () => {
+      if (window.ethereum) {
+        setIsProviderInjected(true);
+      }
+      const pvdr = new BrowserProvider(window.ethereum);
+      setProvider(pvdr);
+    };
+
+    onLoad();
+  }, []);
+
+  useEffect(() => {
+    setTxnPending(false);
+  }, [receipt]);
+
+  const handleConnectClick = () => {
+    if (walletAddress !== "") {
+      window.location.reload();
+    } else {
+      provider?.send("eth_requestAccounts", []).then((res) => {
+        console.log("Connected to", res);
+        setWalletAddress(res[0]);
+      });
+    }
+  };
+
+  const handlePayment = async () => {
+    setTxnPending(true);
+    const signer = await provider?.getSigner();
+    const usdcContract = new Contract(
+      USDC_MUMBAI_CONTRACT_ADDRESS,
+      USDC_ABI,
+      signer
+    );
+    const tokenDecimals = await usdcContract.decimals();
+    const amountToTransferInWei = parseUnits("5.0", tokenDecimals);
+
+    const tx = await usdcContract.transfer(
+      walletAddress,
+      amountToTransferInWei
+    );
+    const result = await tx.wait();
+    console.log(result);
+    setReceipt(result);
+  };
+
+  return (
+    <main className="min-h-screen w-screen bg-black flex flex-col items-center p-24 gap-8">
+      <section className="flex flex-col items-center">
+        <div className="py-2">
+          <p className="text-4xl py-2 text-center">ka-ching.io prototype</p>
+          <p className="text-center">
+            window.ethereum {isProviderInjected ? "" : "not"} found.
+          </p>
+          <p className="text-center">
+            Connected to {walletAddress !== "" ? walletAddress : "null"}.
+          </p>
+        </div>
+        <button
+          disabled={!isProviderInjected}
+          className="rounded-lg px-3 py-2 font-semibold bg-gray-900"
+          onClick={handleConnectClick}
+        >
+          {walletAddress !== "" ? "Disconnect" : "Connect Wallet"}
+        </button>
+      </section>
+      <section className="flex flex-col items-center">
+        <p className="font-bold text-2xl">Transaction Details</p>
+        <p className="py-2">
+          You need to pay
+          <strong>{" 5 USD "}</strong>
+          to complete the transaction.
+        </p>
+        <button
+          disabled={!(isProviderInjected && walletAddress !== "")}
+          className="rounded-lg px-3 py-2 bg-gray-900"
+          onClick={handlePayment}
+        >
+          {isProviderInjected && walletAddress !== ""
+            ? "Send 5 USDC on Polygon_Mumbai"
+            : "Wallet not connected to make payment."}
+        </button>
+      </section>
+      <section className="flex flex-col items-center justify-center">
+        <p className="font-bold text-2xl py-4">Transaction Receipt</p>
+        {txnPending ? (
+          <Loader />
+        ) : (
+          <a
+            className="text-white underline"
+            target="_blank"
+            href={`https://mumbai.polygonscan.com/tx/${receipt.hash}`}
+          >
+            {receipt.hash}
+          </a>
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/constants.ts
@@ -0,0 +1,9 @@
+export const USDC_MUMBAI_CONTRACT_ADDRESS =
+  "0x9999f7Fea5938fD3b1E26A12c3f2fb024e194f97";
+export const USDC_ABI = [
+  "function decimals() view returns (uint8)",
+  "function symbol() view returns (string)",
+  "function balanceOf(address a) view returns (uint)",
+  "function transfer(address to, uint amount) returns (bool)",
+];
+export const DEPOSIT_ADDRESS = "0xd19EdD84F9770D573cFa02f37A45938CB8CD7B5b";
